0404bd98e38a776a0ec5ab8265c617525b814d26
mailhexu <mailhexu@gmail.com>
2024-04-19 17:28:32 +0200

    small changes

---
+++ lawaf/gui/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from nicegui import app, ui
+
+ui.label("Hello, world!")
+ui.button("Click me", on_click=lambda: ui.label("Thanks!"))
+with ui.pyplot():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plt.plot(x, y)
+
+
+ui.run()
+
+
+# ui.label('Lawaf!')
+# ui.run()
